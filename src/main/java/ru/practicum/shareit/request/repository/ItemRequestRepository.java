7697709817b4dc8196335529fb92f6acb3632cfa
zZSerg1iq <142403732+zZSerg1iq@users.noreply.github.com>
2024-04-05 22:32:19 +0500

    Delete src/main/java/ru/practicum/shareit directory

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.request.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.request.model.ItemRequest;
-
-@Repository
-public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
-}
